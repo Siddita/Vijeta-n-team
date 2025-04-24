0dcbd4a7ad14c4d470178878f1a21bd611bc3b70
ShreerajM07 <shreerajbm2482@gmail.com>
2025-04-24 17:24:50 +0530

    Delete test_post.py

--- test_post.py
+++
@@ -1,8 +0,0 @@
-import json, requests
-
-with open("sample_data/patient1.json") as f:
-    payload = json.load(f)
-
-resp = requests.post("http://127.0.0.1:8000/input", json=payload)
-print(resp.status_code)
-print(resp.json())
