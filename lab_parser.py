956cc73282cbac2d04bcb8316f7574208621ffa7
ShreerajM07 <shreerajbm2482@gmail.com>
2025-04-24 17:24:10 +0530

    Delete lab_parser.py

--- lab_parser.py
+++
@@ -1,8 +0,0 @@
-# Lab Parser
-def analyze_lab(crp, wbc):
-    if crp > 10 and wbc > 11000:
-        return "Infection likely"
-    elif crp <= 10 and wbc <= 11000:
-        return "Normal"
-    else:
-        return "Further analysis needed"
